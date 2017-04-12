b1b8c4e63b0c9c5b5314490584fce61f3bc131f2
Devashish <devashish.86@gmail.com>
2017-04-12 11:58:30 -0700

    adding GET api/db/logs route

--- api/db/Routes.js
+++ api/db/Routes.js
@@ -5,5 +5,6 @@ var validateAccount = require('../../common/auth/validateAccount.js');
 
 function dbRoutes(app) {
   app.get('/api/db', validateAccount, require('./get.js'));
+  app.get('/api/db/logs', validateAccount, require('./getLogs.js'));
   app.post('/api/db', validateAccount, require('./post.js'));
 }
