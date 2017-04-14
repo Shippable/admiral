76a179c63500832ff595f050430620e07472a74c
Aidan Murphy <aidan@shippable.com>
2017-04-13 20:56:15 -0700

    #207; update POST /api/redis.

--- api/redis/post.js
+++ api/redis/post.js
@@ -10,6 +10,7 @@ var fs = require('fs');
 var spawn = require('child_process').spawn;
 
 var APIAdapter = require('../../common/APIAdapter.js');
+var configHandler = require('../../common/configHandler.js');
 
 function post(req, res) {
   var bag = {
@@ -17,6 +18,7 @@ function post(req, res) {
     resBody: [],
     apiAdapter: new APIAdapter(req.headers.authorization.split(' ')[1]),
     params: {},
+    skipStatusChange: false,
     component: 'redis',
     tmpScript: '/tmp/redis.sh'
   };
@@ -27,6 +29,7 @@ function post(req, res) {
   async.series([
       _checkInputParams.bind(null, bag),
       _get.bind(null, bag),
+      _setProcessingFlag.bind(null, bag),
       _generateInitializeEnvs.bind(null, bag),
       _generateInitializeScript.bind(null, bag),
       _writeScriptToFile.bind(null, bag),
@@ -36,6 +39,9 @@ function post(req, res) {
     ],
     function (err) {
       logger.info(bag.who, 'Completed');
+      if (!bag.skipStatusChange)
+        _setCompleteStatus(bag, err);
+
       if (err)
         return respondWithError(res, err);
 
@@ -56,32 +62,49 @@ function _get(bag, next) {
   var who = bag.who + '|' + _get.name;
   logger.verbose(who, 'Inside');
 
-  var query = util.format('SELECT %s from "systemConfigs"', bag.component);
-  global.config.client.query(query,
-    function (err, systemConfigs) {
-      if (err)
+  configHandler.get(bag.component,
+    function (err, redis) {
+      if (err) {
+        bag.skipStatusChange = true;
         return next(
           new ActErr(who, ActErr.DataNotFound, err)
         );
+      }
 
-      if (!_.isEmpty(systemConfigs.rows) &&
-        !_.isEmpty(systemConfigs.rows[0].redis)) {
-        logger.debug('Found configuration for ' + bag.component);
-
-        bag.config = systemConfigs.rows[0].redis;
-        bag.config = JSON.parse(bag.config);
-      } else {
+      if (_.isEmpty(redis)) {
+        bag.skipStatusChange = true;
         return next(
           new ActErr(who, ActErr.DataNotFound,
-            'No configuration present in configs for ' + bag.component)
+            'No configuration in database for ' + bag.component)
         );
       }
 
+      bag.config = redis;
       return next();
     }
   );
 }
 
+function _setProcessingFlag(bag, next) {
+  var who = bag.who + '|' + _setProcessingFlag.name;
+  logger.verbose(who, 'Inside');
+
+  var update = {
+    isProcessing: true,
+    isFailed: false
+  };
+
+  configHandler.put(bag.component, update,
+    function (err) {
+      if (err)
+        return next(
+          new ActErr(who, ActErr.OperationFailed, err)
+        );
+
+      return next();
+    }
+  );
+}
 function _generateInitializeEnvs(bag, next) {
   var who = bag.who + '|' + _generateInitializeEnvs.name;
   logger.verbose(who, 'Inside');
@@ -202,28 +225,39 @@ function _post(bag, next) {
   var who = bag.who + '|' + _post.name;
   logger.verbose(who, 'Inside');
 
-  var update = bag.config;
-  bag.config.isInstalled = true;
-  bag.config.isInitialized = true;
+  var update = {
+    isInstalled: true,
+    isInitialized: true
+  };
 
-  var query = util.format('UPDATE "systemConfigs" set %s=\'%s\';',
-    bag.component, JSON.stringify(update));
-
-  global.config.client.query(query,
-    function (err, response) {
+  configHandler.put(bag.component, update,
+    function (err) {
       if (err)
         return next(
-          new ActErr(who, ActErr.DataNotFound, err)
+          new ActErr(who, ActErr.OperationFailed, err)
         );
 
-      if (response.rowCount === 1) {
-        logger.debug('Successfully added default value for ' + bag.component);
-        bag.resBody = update;
-      } else {
-        logger.warn('Failed to set default value for ' + bag.component);
-      }
-
       return next();
+    }
+  );
+}
+
+function _setCompleteStatus(bag, err) {
+  var who = bag.who + '|' + _setCompleteStatus.name;
+  logger.verbose(who, 'Inside');
+
+  var update = {
+    isProcessing: false
+  };
+  if (err)
+    update.isFailed = true;
+  else
+    update.isFailed = false;
+
+  configHandler.put(bag.component, update,
+    function (err) {
+      if (err)
+        logger.warn(err);
     }
   );
 }
