f7022aed3c8c398cc0a6a0c301151bf0ee5a7866
stephanielingwood <stephanielingwood@gmail.com>
2017-04-16 15:14:32 -0700

    Adds GET providers

--- api/providers/Routes.js
+++ api/providers/Routes.js
@@ -4,5 +4,6 @@ module.exports = providersRoutes;
 var validateAccount = require('../../common/auth/validateAccount.js');
 
 function providersRoutes(app) {
+  app.get('/api/providers', validateAccount, require('./get.js'));
   app.post('/api/providers', validateAccount, require('./post.js'));
 }
