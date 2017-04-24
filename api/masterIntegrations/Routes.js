e9ac8e8dc54bc30233035955e115c7c9ba6ccbbd
Devashish <devashish.86@gmail.com>
2017-04-24 16:01:54 -0700

    adding GET /api/masterIntegrations route

--- api/masterIntegrations/Routes.js
+++ api/masterIntegrations/Routes.js
@@ -4,6 +4,8 @@ module.exports = masterIntegrationRoutes;
 var validateAccount = require('../../common/auth/validateAccount.js');
 
 function masterIntegrationRoutes(app) {
+  app.get('/api/masterIntegrations', validateAccount,
+    require('./get.js'));
   app.put('/api/masterIntegrations/:masterIntegrationId', validateAccount,
     require('./put.js'));
 }
