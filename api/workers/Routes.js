e5f8a261a39a761af22b3c93eff237602fba0406
Devashish <devashish.86@gmail.com>
2017-05-08 14:54:34 -0700

    adding workers post route

--- api/workers/Routes.js
+++ api/workers/Routes.js
@@ -5,4 +5,5 @@ var validateAccount = require('../../common/auth/validateAccount.js');
 
 function workerRoutes(app) {
   app.get('/api/workers', validateAccount, require('./get.js'));
+  app.post('/api/workers', validateAccount, require('./post.js'));
 }
