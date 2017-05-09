2b044afff37b9cdd629d6ec510cf876e8ace1123
Devashish <devashish.86@gmail.com>
2017-05-08 17:11:48 -0700

    adding scripts to install master

--- api/master/Routes.js
+++ api/master/Routes.js
@@ -6,4 +6,5 @@ var validateAccount = require('../../common/auth/validateAccount.js');
 function masterRoutes(app) {
   app.get('/api/master', validateAccount, require('./get.js'));
   app.post('/api/master', validateAccount, require('./post.js'));
+  app.post('/api/master/initialize', validateAccount, require('./initialize.js'));
 }
---
+++ common/scripts/installMaster.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+export COMPONENT="master"
+export MASTER_DATA_DIR="$RUNTIME_DIR/$COMPONENT/data"
+export MASTER_CONFIG_DIR="$CONFIG_DIR/$COMPONENT"
+export SCRIPTS_DIR="$SCRIPTS_DIR"
+export LOGS_FILE="$RUNTIME_DIR/logs/$COMPONENT.log"
+export SCRIPTS_DIR_REMOTE="/tmp/shippable"
+
+## Write logs of this script to component specific file
+exec &> >(tee -a "$LOGS_FILE")
+
+__validate_master_envs() {
+  __process_msg "Initializing redis environment variables"
+  __process_msg "SCRIPTS_DIR: $SCRIPTS_DIR"
+  __process_msg "MASTER_HOST: $MASTER_HOST"
+  __process_msg "MASTER_PORT: $MASTER_PORT"
+}
+
+__init_swarm_master() {
+  __process_msg "Initializing swarm cluster"
+  sudo docker swarm init || true
+}
+
+main() {
+  __process_marker "Installing swarm master"
+  __init_swarm_master
+  __process_msg "Swarm master installed successfully"
+}
+
+main
