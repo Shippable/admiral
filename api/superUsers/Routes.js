34ee8543d34e50a68dd3532cd99f7d3d8aa489b9
John Stockbauer <john@shippable.com>
2017-05-11 17:26:34 -0700

    add route for getting superuser list

--- api/superUsers/Routes.js
+++ api/superUsers/Routes.js
@@ -5,4 +5,5 @@ var validateAccount = require('../../common/auth/validateAccount.js');
 
 function superUserRoutes(app) {
   app.post('/api/superusers', validateAccount, require('./postSuperUsers.js'));
+  app.get('/api/superusers', validateAccount, require('./get.js'));
 }
