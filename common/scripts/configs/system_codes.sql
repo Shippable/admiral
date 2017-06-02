935b818497d5c54a0b50de1431d44f68819c966f
ankul-shippable <ankul@shippable.com>
2017-06-02 17:42:33 +0530

    add systemCode for showGridView

--- common/scripts/configs/system_codes.sql
+++ common/scripts/configs/system_codes.sql
@@ -541,6 +541,11 @@ do $$
       values (405, 'showDeletedResources', 'viewObjectType', '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2016-06-01', '2016-06-01');
     end if;
 
+    if not exists (select 1 from "systemCodes" where code = 406) then
+      insert into "systemCodes" ("code", "name", "group", "createdBy", "updatedBy", "createdAt", "updatedAt")
+      values (406, 'showGridView', 'viewObjectType', '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2016-06-01', '2016-06-01');
+    end if;    
+
     if not exists (select 1 from "systemCodes" where code = 500) then
       insert into "systemCodes" ("code", "name", "group", "createdBy", "updatedBy", "createdAt", "updatedAt")
       values (500, 'accountDashboard', 'viewType', '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2016-06-01', '2016-06-01');
