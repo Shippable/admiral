f2b25201a65652a148d8d5c774ee9d8111d11064
Devashish <devashish.86@gmail.com>
2017-06-19 13:00:27 -0700

    unsealing vault

--- common/scripts/restart.sh
+++ common/scripts/restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+export TIMEOUT=60
+
 __check_admiral() {
   __process_msg "Checking if admiral container is running"
   local wait_time=3
@@ -34,6 +36,7 @@ __start_secrets() {
     if [ "$secrets_container" != "" ]; then
       __process_msg "Found a stopped secrets container, starting it"
       sudo docker start $secrets_container
+      sleep 3
     else
       __process_error "No secrets container found in stopped state, exiting"
       exit 1
@@ -43,11 +46,81 @@ __start_secrets() {
   fi
 }
 
+__check_secrets_status() {
+  __process_msg "Getting secrets status"
+
+  local secrets=""
+  _shippable_get_secrets_status
+  if [ $response_status_code -gt 299 ]; then
+    __process_error "Error getting secrets: $response"
+    __process_error "Status code: $response_status_code"
+    exit 1
+  else
+    __process_msg "Successfully fetched secrets status"
+    secrets=$(echo $response | jq '.')
+  fi
+
+  local is_reachable=$(echo $secrets | jq -r '.isReachable')
+  if [ $is_reachable == false ]; then
+    __process_error "Secrets store not reachable, exiting"
+    exit 1
+  else
+    __process_msg "Secrets store reachable"
+  fi
+}
+
+__unseal() {
+  __process_msg "Unsealing secrets store"
+
+  _shippable_post_secrets_initialize
+  if [ $response_status_code -gt 299 ]; then
+    __process_error "Error unsealing secrets store: $response"
+    __process_error "Status code: $response_status_code"
+    exit 1
+  else
+    __process_msg "Successfully called secrects unseal route"
+  fi
+
+}
+
+__check_unseal_status() {
+  __process_msg "Checking if secrets store is unsealed"
+  local interval=3
+  local counter=0
+  local sealed_status=true
+
+  while [ $sealed_status == true ] && [ $counter -lt $TIMEOUT ]; do
+    local secrets=""
+    _shippable_get_secrets_status
+    if [ $response_status_code -gt 299 ]; then
+      __process_error "Error getting secrets: $response"
+      __process_error "Status code: $response_status_code"
+      exit 1
+    else
+      __process_msg "Successfully fetched secrets status"
+      secrets=$(echo $response | jq '.')
+    fi
+
+    sealed_status=$(echo $secrets | jq -r '.sealed')
+    if [ $sealed_status == false ]; then
+      __process_msg "Secrets store successfully unsealed"
+      break
+    else
+      __process_msg "Secrets store in sealed state, waiting"
+      let "counter = $counter + $interval"
+      sleep $interval
+    fi
+  done
+}
+
 main() {
   if [ $IS_RESTART == true ]; then
     __process_marker "Restarting Shippable services"
     __check_admiral
     __start_secrets
+    __check_secrets_status
+    __unseal
+    __check_unseal_status
   fi
 }
 
