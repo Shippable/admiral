a1084d53ea190e7e9e342b7f4db7087c32562c51
Amal Francis <amalfra@gmail.com>
2017-08-01 16:21:10 +0530

    Create PUT /api/secrets route which allows updating address, port, isShippableManaged, isProcessing, isFailed, isInstalled, isInitialized fields of secrets systemSettings

--- api/secrets/Routes.js
+++ api/secrets/Routes.js
@@ -8,6 +8,7 @@ function secretsRoutes(app) {
   app.get('/api/secrets/status', validateAccount, require('./getStatus.js'));
   app.get('/api/secrets/logs', validateAccount, require('./getLogs.js'));
   app.post('/api/secrets', validateAccount, require('./post.js'));
+  app.put('/api/secrets', validateAccount, require('./put.js'));
   app.post('/api/secrets/initialize', validateAccount,
     require('./initialize.js'));
 }
