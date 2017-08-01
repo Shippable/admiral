1e35408d939cd5ebfa0adeb3aef3bd1e1214400d
Amal Francis <amalfra@gmail.com>
2017-08-01 15:49:32 +0530

    Create PUT /api/msg route which allows updating address, amqpPort, adminPort, isSecure, isShippableManaged, isProcessing, isFailed, isInstalled, isInitialized fields of msg systemSettings

--- api/msg/Routes.js
+++ api/msg/Routes.js
@@ -8,5 +8,6 @@ function msgRoutes(app) {
   app.get('/api/msg/logs', validateAccount, require('./getLogs.js'));
   app.get('/api/msg/status', validateAccount, require('./getStatus.js'));
   app.post('/api/msg', validateAccount, require('./post.js'));
+  app.put('/api/msg', validateAccount, require('./put.js'));
   app.post('/api/msg/initialize', validateAccount, require('./initialize.js'));
 }
