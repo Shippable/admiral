b5ad89de838fa8e1964a6cfbfe279ed7c2a9be42
Amal Francis <amalfra@gmail.com>
2017-10-04 12:25:14 +0530

    add quayLogin master integration

--- common/scripts/configs/master_integrations.sql
+++ common/scripts/configs/master_integrations.sql
@@ -377,6 +377,12 @@ do $$
       values ('596d9b49fa1a3f979c10b5a7', 73, 'artifactoryKey', 'JFrog Artifactory', 'generic', false, 'account', 5012, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2016-06-01', '2016-06-01');
     end if;
 
+    -- adds quayLogin integration
+    if not exists (select 1 from "masterIntegrations" where "name" = 'quayLogin' and "typeCode" = 5012) then
+      insert into "masterIntegrations" ("id", "masterIntegrationId", "name", "displayName", "type", "isEnabled", "level", "typeCode", "createdBy", "updatedBy", "createdAt", "updatedAt")
+      values ('580ee981a337bd12008fc43f', 74, 'quayLogin', 'Quay.io', 'generic', false, 'account', 5012, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2017-10-04', '2017-10-04');
+    end if;
+
     -- END adding master integrations
 
     -- Remove masterIntegrations
