198ea715316cc73ba9e73aaf1369eb7ab72dea2d
deepikasl <deepika@shippable.com>
2017-11-02 15:48:38 +0530

    deprecate AZURE_DCOS int

--- common/scripts/configs/master_integrations.sql
+++ common/scripts/configs/master_integrations.sql
@@ -572,5 +572,10 @@ do $$
     if exists (select 1 from "masterIntegrations" where "name" = 'DDC' and "typeCode" = 5002 and "isDeprecated" = false) then
       update "masterIntegrations" set "isDeprecated" = true where "name" = 'DDC' and "typeCode" = 5002;
     end if;
+
+    --- Deprecate AZURE_DCOS integration
+    if exists (select 1 from "masterIntegrations" where "name" = 'AZURE_DCOS' and "typeCode" = 5002 and "isDeprecated" = false) then
+      update "masterIntegrations" set "isDeprecated" = true where "name" = 'AZURE_DCOS' and "typeCode" = 5002;
+    end if;
   end
 $$;
