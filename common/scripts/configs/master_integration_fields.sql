7f8ce0d7020090663225560782abd5227116f1e4
deepikasl <deepika@shippable.com>
2017-11-03 11:40:41 +0530

    chnage the display name to Webhook

--- common/scripts/configs/master_integration_fields.sql
+++ common/scripts/configs/master_integration_fields.sql
@@ -1004,19 +1004,15 @@ do $$
     end if;
 
     -- masterIntegrationFields for webhookV2
-    if not exists (select 1 from "masterIntegrationFields" where "id" = 259) then
-      insert into "masterIntegrationFields" ("id", "masterIntegrationId", "name", "dataType", "isRequired", "isSecure","createdBy", "updatedBy", "createdAt", "updatedAt")
-      values (259, '59e71aa80552d20500e76ba9', 'project', 'string', false, false, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2017-11-03', '2017-11-03');
-    end if;
 
     if not exists (select 1 from "masterIntegrationFields" where "id" = 260) then
       insert into "masterIntegrationFields" ("id", "masterIntegrationId", "name", "dataType", "isRequired", "isSecure","createdBy", "updatedBy", "createdAt", "updatedAt")
-      values (260, '59e71aa80552d20500e76ba9', 'webhookURL', 'string', false, false, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2017-11-03', '2017-11-03');
+      values (259, '59e71aa80552d20500e76ba9', 'webhookURL', 'string', false, false, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2017-11-03', '2017-11-03');
     end if;
 
     if not exists (select 1 from "masterIntegrationFields" where "id" = 261) then
       insert into "masterIntegrationFields" ("id", "masterIntegrationId", "name", "dataType", "isRequired", "isSecure","createdBy", "updatedBy", "createdAt", "updatedAt")
-      values (261, '59e71aa80552d20500e76ba9', 'authorization', 'string', false, false, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2017-11-03', '2017-11-03');
+      values (260, '59e71aa80552d20500e76ba9', 'authorization', 'string', false, false, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2017-11-03', '2017-11-03');
     end if;
 
     -- END adding master integration fields
--- common/scripts/configs/master_integrations.sql
+++ common/scripts/configs/master_integrations.sql
@@ -439,7 +439,7 @@ do $$
     -- webhookV2
     if not exists (select 1 from "masterIntegrations" where "name" = 'webhookV2' and "typeCode" = 5012) then
       insert into "masterIntegrations" ("id", "masterIntegrationId", "name", "displayName", "type", "isEnabled", "level", "typeCode", "createdBy", "updatedBy", "createdAt", "updatedAt")
-      values ('59e71aa80552d20500e76ba9', 83, 'webhookV2', 'Event Trigger', 'generic', false, 'account', 5012, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2017-11-03', '2017-11-03');
+      values ('59e71aa80552d20500e76ba9', 83, 'webhookV2', 'Webhook', 'generic', false, 'account', 5012, '54188262bc4d591ba438d62a', '54188262bc4d591ba438d62a', '2017-11-03', '2017-11-03');
     end if;
 
     -- END adding master integrations
