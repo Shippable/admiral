588a94e65786530c981467fd3841b5a9fb4428fe
deepikasl <deepika@shippable.com>
2017-11-16 15:40:02 +0530

    add routeRoles for strictJobs

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -2859,6 +2859,49 @@ do $$
       roleCode := 6060
     );
 
+    -- set strictJobs routeRoles
+
+    perform set_route_role(
+      routePattern := '/strictJobs',
+      httpVerb := 'GET',
+      roleCode := 6000
+    );
+
+    perform set_route_role(
+      routePattern := '/strictJobs',
+      httpVerb := 'GET',
+      roleCode := 6010
+    );
+
+    perform set_route_role(
+      routePattern := '/strictJobs',
+      httpVerb := 'GET',
+      roleCode := 6020
+    );
+
+    perform set_route_role(
+      routePattern := '/strictJobs',
+      httpVerb := 'GET',
+      roleCode := 6060
+    );
+
+    perform set_route_role(
+      routePattern := '/strictJobs/:strictJobId',
+      httpVerb := 'DELETE',
+      roleCode := 6010
+    );
+
+    perform set_route_role(
+      routePattern := '/strictJobs/:strictJobId',
+      httpVerb := 'DELETE',
+      roleCode := 6020
+    );
+
+    perform set_route_role(
+      routePattern := '/strictJobs/:strictJobId',
+      httpVerb := 'DELETE',
+      roleCode := 6060
+    );
     -- set subscriptions routeRoles
 
     perform set_route_role(
