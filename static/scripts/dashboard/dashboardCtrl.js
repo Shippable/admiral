084a42df3312b8a7cf9b6e4d88fefe2dba42219d
deepikasl <deepika@shippable.com>
2018-01-11 12:26:47 +0530

    use regex to validate url instead of using angular

--- static/scripts/dashboard/dashboardCtrl.js
+++ static/scripts/dashboard/dashboardCtrl.js
@@ -51,6 +51,7 @@
         'internalAPI',
         'consoleAPI'
       ],
+      isUrlNotValid: [],
       x86ArchCode: null,
       armArchCode: null,
       initializeForm: {
@@ -98,6 +99,15 @@
         sshCommand: '',
         installerAccessKey: '',
         installerSecretKey: ''
+      },
+      formValuesChanged: function (url, name) {
+        var urlRegex = /(http(s)?:\/\/)?([a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+.*)$/;
+        
+        if(urlRegex.test(url)) {
+          $scope.vm.isUrlNotValid[name] = false;
+        }
+        else
+          $scope.vm.isUrlNotValid[name] = true;
       },
       // map by systemInt name, then masterName
       installForm: {
