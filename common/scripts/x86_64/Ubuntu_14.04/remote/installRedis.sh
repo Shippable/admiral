0cafe6468c3f9d97c84adca032b934b0c91ead92
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-02-20 13:55:25 +0530

    adds default config options

--- common/scripts/x86_64/Ubuntu_14.04/remote/installRedis.sh
+++ common/scripts/x86_64/Ubuntu_14.04/remote/installRedis.sh
@@ -9,5 +9,9 @@ pushd redis-stable
   make
   make test
   sudo make install
-  sudo ./utils/install_server.sh
+  sudo REDIS_PORT=6379 \
+    REDIS_CONFIG_FILE=/etc/redis/6379.conf \
+    REDIS_LOG_FILE=/var/log/redis_6379.log \
+    REDIS_DATA_DIR=/var/lib/redis/6379 \
+    REDIS_EXECUTABLE=`command -v redis-server` ./utils/install_server.sh
 popd
