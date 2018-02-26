407fa2f93aeb16e310b7e819fb290c1b45752d45
Shrivara <shrivara.jois@gmail.com>
2018-02-26 17:50:03 +0530

    Add CentOS_7 helpers required to initialize DB

--- common/scripts/x86_64/CentOS_7/_helpers.sh
+++ common/scripts/x86_64/CentOS_7/_helpers.sh
@@ -428,3 +428,99 @@ __check_service_connection() {
     exit 1
   fi
 }
+
+__add_ssh_key_to_db() {
+  if [ "$DB_IP" != "$ADMIRAL_IP" ] && [ "$DB_INSTALLED" == "false" ]; then
+    local public_ssh_key=$(cat $SSH_PUBLIC_KEY)
+    __process_success "Run the following command on $DB_IP to allow SSH access:"
+
+    echo 'sudo mkdir -p /root/.ssh; echo '$public_ssh_key' | sudo tee -a /root/.ssh/authorized_keys;'
+
+    __process_success "Enter Y to confirm that you have run this command"
+    read confirmation
+    confirmation=$(echo $confirmation | awk '{print toupper($0)}')
+
+    if [[ "$confirmation" =~ "Y" ]]; then
+      __process_msg "Confirmation received"
+    else
+      __process_error "Invalid response, please run the command to allow access and continue"
+      __add_ssh_key_to_db
+    fi
+  fi
+}
+
+__check_connection() {
+  if [ "$#" -ne 1 ]; then
+    __process_error "At least one host name required to check connection"
+    exit 1
+  fi
+  local host="$1"
+  __process_msg "Checking connection status for : $host"
+  __exec_cmd_remote "$host" "echo 'Successfully pinged $host'"
+}
+
+__exec_cmd_remote() {
+  local user="$SSH_USER"
+  local key="$SSH_PRIVATE_KEY"
+  local timeout=10
+  local port=22
+
+  local host="$1"
+  shift
+  local cmd="$@"
+
+  local remote_cmd="ssh \
+    -o StrictHostKeyChecking=no \
+    -o NumberOfPasswordPrompts=0 \
+    -o ConnectTimeout=$timeout \
+    -p $port \
+    -i $key \
+    $user@$host \
+    \"$cmd\""
+
+  {
+    __process_msg "Executing on host: $host ==> '$cmd'" && eval "sudo -E $remote_cmd"
+  } || {
+    __process_msg "ERROR: Command failed on host: $host ==> '$cmd'"
+    exit 1
+  }
+}
+
+__copy_script_remote() {
+  if [ "$#" -ne 3 ]; then
+    __process_msg "The number of arguments expected by _copy_script_remote is 3"
+    __process_msg "current arguments $@"
+    exit 1
+  fi
+
+  local user="$SSH_USER"
+  local key="$SSH_PRIVATE_KEY"
+  local port=22
+  local host="$1"
+  shift
+  local script_path_local="$1"
+  local script_name=$(basename $script_path_local)
+  shift
+  local script_dir_remote="$1"
+  local script_path_remote="$script_dir_remote/$script_name"
+
+  remove_key_cmd="ssh-keygen -q -f '$HOME/.ssh/known_hosts' -R $host > /dev/null 2>&1"
+  {
+    eval $remove_key_cmd
+  } || {
+    true
+  }
+
+  __process_msg "Copying $script_path_local to remote host: $script_path_remote"
+  __exec_cmd_remote $host "mkdir -p $script_dir_remote"
+  copy_cmd="rsync -avz -e \
+    'ssh \
+      -o StrictHostKeyChecking=no \
+      -o NumberOfPasswordPrompts=0 \
+      -p $port \
+      -i $SSH_PRIVATE_KEY \
+      -C' \
+      $script_path_local $user@$host:$script_path_remote"
+
+  copy_cmd_out=$(eval $copy_cmd)
+}
