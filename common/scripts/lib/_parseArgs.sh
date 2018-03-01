a4129c63f583225ccf06299587fbded4cf178da7
Bharath Upadhya <1992bharath@gmail.com>
2018-03-01 16:07:00 +0530

    change authorized keys only after shippable license is accepted

--- common/scripts/lib/_parseArgs.sh
+++ common/scripts/lib/_parseArgs.sh
@@ -308,18 +308,20 @@ __prompt_for_inputs() {
 }
 
 __set_host_services() {
-  sed -i '/^HOST_SERVICES/d' "$ADMIRAL_ENV"
-  echo "HOST_SERVICES=true" >> "$ADMIRAL_ENV"
-  source "$ADMIRAL_ENV"
+  if [ "$HOST_SERVICES" == "true" ]; then
+    sed -i '/^HOST_SERVICES/d' "$ADMIRAL_ENV"
+    echo "HOST_SERVICES=true" >> "$ADMIRAL_ENV"
+    source "$ADMIRAL_ENV"
 
-  # create .ssh dir, if not present
-  local root_ssh_dir="/root/.ssh"
-  mkdir -p "$root_ssh_dir"
+    # create .ssh dir, if not present
+    local root_ssh_dir="/root/.ssh"
+    mkdir -p "$root_ssh_dir"
 
-  ## add public ssh key to host's authorized keys
-  local authorized_keys="$root_ssh_dir/authorized_keys"
-  local ssh_public_key=$(cat "$SSH_PUBLIC_KEY")
-  echo "$ssh_public_key" | tee -a "$authorized_keys" &> /dev/null
+    ## add public ssh key to host's authorized keys
+    local authorized_keys="$root_ssh_dir/authorized_keys"
+    local ssh_public_key=$(cat "$SSH_PUBLIC_KEY")
+    echo "$ssh_public_key" | tee -a "$authorized_keys" &> /dev/null
+  fi
 }
 
 __parse_args_install() {
@@ -340,7 +342,7 @@ __parse_args_install() {
           export WITH_PROXY_CONFIG=true
           ;;
         -H|--host_services)
-          __set_host_services
+          export HOST_SERVICES=true
           ;;
         -h|help|--help)
           __print_help_install
@@ -544,6 +546,7 @@ __parse_args() {
         __generate_ssh_keys
         __parse_args_install "$@"
         __accept_shippable_license
+        __set_host_services
         ;;
       switch)
         if [ $# -gt 1 ]; then
