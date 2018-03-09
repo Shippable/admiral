5c1846bb657a63112eba7841d9f2da701a0f8fe5
Riturajcse <riturajcse1994@gmail.com>
2018-03-09 14:20:41 +0530

    adds addSharedNode script to add default system cluster and system node

--- api/systemNodes/get.js
+++ api/systemNodes/get.js
@@ -8,6 +8,7 @@ var _ = require('underscore');
 
 function get(req, res) {
   var bag = {
+    reqQuery: req.query,
     resBody: []
   };
 
@@ -16,6 +17,7 @@ function get(req, res) {
 
   async.series([
       _checkInputParams.bind(null, bag),
+      _constructQuery.bind(null, bag),
       _get.bind(null, bag)
     ],
     function (err) {
@@ -35,12 +37,29 @@ function _checkInputParams(bag, next) {
   return next();
 }
 
+function _constructQuery(bag, next) {
+  var who = bag.who + '|' + _constructQuery.name;
+  logger.verbose(who, 'Inside');
+
+  // Can enhance the queries for other params if required in future.
+  var query = 'SELECT * FROM "systemNodes"';
+  var queries = [];
+
+  if (_.has(bag.reqQuery, 'systemClusterId'))
+    queries.push(util.format('"systemClusterId"=\'%s\'', bag.reqQuery.systemClusterId));
+
+  if (queries.length)
+    query = query + ' WHERE ' + queries.join(' AND ');
+
+  bag.query = query;
+  return next();
+}
+
 function _get(bag, next) {
   var who = bag.who + '|' + _get.name;
   logger.verbose(who, 'Inside');
 
-  var query = 'SELECT * FROM "systemNodes"';
-  global.config.client.query(query,
+  global.config.client.query(bag.query,
     function (err, systemNodes) {
       if (err)
         return next(
