bd34efa85e2ce2ebcc5d367e4de6c4026a4e5c25
Riturajcse <riturajcse1994@gmail.com>
2018-03-12 15:23:29 +0530

    remove existing incorrect folder

--- common/scripts/lib/_helpers.sh
+++ common/scripts/lib/_helpers.sh
@@ -34,6 +34,7 @@ __check_os_and_architecture() {
     # it as "Ubuntu" so that will not change here.
     local os=$(echo "$NAME" | cut -f 1 -d ' ')
     local os_version="$VERSION_ID"
+    echo "#######################os##########################"
     if [ os == "RED"]; then
       $os="RHEL"
       $os_version=$(echo "$VERSION_ID" | cut -f 1 -d '.')
