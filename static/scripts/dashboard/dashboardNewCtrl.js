3d898c5df7a6341d67a628ec57070a5c14756e94
deepikasl <deepika@shippable.com>
2018-03-16 23:17:12 +0530

    update stateUpdate.sshPort as per the changes

--- static/scripts/dashboard/dashboardNewCtrl.js
+++ static/scripts/dashboard/dashboardNewCtrl.js
@@ -2247,9 +2247,8 @@
 
       // Set the correct port if GitLab has already been initialized,
       // even if it's Shippable managed:
-      if ($scope.vm.systemSettings.state.isInitialized)
-        stateUpdate.sshPort = $scope.vm.initializeForm.state.sshPort ||
-          ($scope.vm.initializeForm.state.initType === 'admiral' ? 2222 : 22);
+      stateUpdate.sshPort = $scope.vm.initializeForm.state.sshPort ||
+        ($scope.vm.admiralEnv.DEV_MODE ? 2222 : 22);
 
       var updateInitializeFormBag = {
         inPostServices: true
