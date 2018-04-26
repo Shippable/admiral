5ce5c0855ac01281928b8053a37701bc79d3ec75
Riturajcse <riturajcse1994@gmail.com>
2018-04-26 16:17:48 +0530

    code review points

--- api/systemIntegrations/post.js
+++ api/systemIntegrations/post.js
@@ -204,7 +204,7 @@ function _validateMasterIntegrationFields(bag, next) {
       if (!_.findWhere(bag.masterIntegrationFields, {name: key})) {
         return next(
           new ActErr(who, ActErr.OperationFailed,
-            util.format('Can not save %s as there is no masterIntegraitonField named %s.', key, key))
+            util.format('Can not save %s as there is no masterIntegrationField named %s.', key, key))
         );
       }
     }
