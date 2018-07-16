828ea7cb72d4f2507638acf2881fc085015bd3c5
Riturajcse <riturajcse1994@gmail.com>
2018-07-16 13:01:36 +0530

    updates node version to v8.11.3 while bringing system nodes in local

--- common/scripts/x86_64/Ubuntu_14.04/install_node.sh
+++ common/scripts/x86_64/Ubuntu_14.04/install_node.sh
@@ -3,12 +3,12 @@
 main() {
 
   pushd /tmp
-  echo "Installing node 4.8.5"
-  sudo wget https://nodejs.org/dist/v4.8.5/node-v4.8.5-linux-x64.tar.xz
-  sudo tar -xf node-v4.8.5-linux-x64.tar.xz
-  sudo cp -Rf node-v4.8.5-linux-x64/{bin,include,lib,share} /usr/local
+  echo "Installing node 8.11.3"
+  sudo wget https://nodejs.org/dist/v8.11.3/node-v8.11.3-linux-x64.tar.xz
+  sudo tar -xf node-v8.11.3-linux-x64.tar.xz
+  sudo cp -Rf node-v8.11.3-linux-x64/{bin,include,lib,share} /usr/local
   node -v
-  sudo rm -rf node-v4.8.5-linux-x64.tar.xz
+  sudo rm -rf node-v8.11.3-linux-x64.tar.xz
   popd
 }
 
