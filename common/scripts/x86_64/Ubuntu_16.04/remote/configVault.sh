5ce707e3ca9b31f7b488dd8c5b5a61e5192d925e
Devashish <devashish.86@gmail.com>
2018-07-25 14:47:29 +0530

    fixing configfile paths

--- common/scripts/x86_64/Ubuntu_16.04/remote/configVault.sh
+++ common/scripts/x86_64/Ubuntu_16.04/remote/configVault.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -e
 
+export VAULT_CONFIG_PATH="/etc/shippable/secrets/scripts"
+export VAULT_SYSTEMD_PATH="/etc/systemd/system/"
+
 __copy_vault_config() {
   __process_msg "Copying vault upstart file"
 
   local vault_config_path="$VAULT_CONFIG_DIR/scripts/vault.service"
-  __copy_script_remote "$VAULT_HOST" "$vault_config_path" "/etc/systemd/system/"
+  __copy_script_remote "$VAULT_HOST" "$vault_config_path" "$VAULT_SYSTEMD_PATH"
 }
 
 __copy_vault_bootstrap() {
   __process_msg "Copying vault bootstrap file"
-  local vault_bootstrap_path="$VAULT_CONFIG_DIR/scripts/vault.service"
+  local vault_bootstrap_path="$VAULT_CONFIG_DIR/scripts/bootstrap_vault.sh"
+  __copy_script_remote "$VAULT_HOST" "$vault_bootstrap_path" "$VAULT_CONFIG_PATH"
 
 }
 
