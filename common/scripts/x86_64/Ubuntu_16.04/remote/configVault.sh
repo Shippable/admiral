223a56938a055916f36330b2df5298927cf21ec4
Devashish <devashish.86@gmail.com>
2018-07-25 14:41:52 +0530

    adding bootstrap script for vault service

--- common/scripts/x86_64/Ubuntu_16.04/remote/configVault.sh
+++ common/scripts/x86_64/Ubuntu_16.04/remote/configVault.sh
@@ -7,4 +7,12 @@ __copy_vault_config() {
   __copy_script_remote "$VAULT_HOST" "$vault_config_path" "/etc/systemd/system/"
 }
 
+__copy_vault_bootstrap() {
+  __process_msg "Copying vault bootstrap file"
+  local vault_bootstrap_path="$VAULT_CONFIG_DIR/scripts/vault.service"
+
+}
+
 __copy_vault_config
+__copy_vault_bootstrap
+
