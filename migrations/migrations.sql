aa4897144e03ef540403f9ee0316e4baa955108f
ankul <ankul@shippable.com>
2018-07-30 23:35:17 +0530

    add default value

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -4590,7 +4590,7 @@ do $$
 
     -- Add isIntegration to subscriptionIntegrations
     if not exists (select 1 from information_schema.columns where table_name = 'subscriptionIntegrations' and column_name = 'isIntegration') then
-      alter table "subscriptionIntegrations" add column "isIntegration" BOOLEAN;
+      alter table "subscriptionIntegrations" add column "isIntegration" BOOLEAN DEFAULT false;
     end if;
 
     -- Add editRoleCode to subscriptionIntegrations
