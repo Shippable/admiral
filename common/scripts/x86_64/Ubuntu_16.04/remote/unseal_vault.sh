fd941a1f69ab1d08e094eb4f81c594a1d2830177
Devashish <devashish.86@gmail.com>
2018-08-01 11:12:54 +0530

    using vault api to unseal vault

--- common/scripts/x86_64/Ubuntu_16.04/remote/unseal_vault.sh
+++ common/scripts/x86_64/Ubuntu_16.04/remote/unseal_vault.sh
@@ -1,33 +1,134 @@
 #! /bin/bash
 set -e
 
-export ADMIRAL_ENV="/etc/shippable/admiral.env"
-export BOOT_WAIT=10
-export VAULT_ADDR='http://127.0.0.1:8200'
+readonly VAULT_API_RESPONSE_FILE="/tmp/.vault_response"
+readonly ADMIRAL_ENV="/etc/shippable/admiral.env"
+readonly BOOT_WAIT=10
+
+##################### Begin Vault adapter ######################################
+
+__initialize() {
+  echo "Initializing vault api adapter"
+  RESPONSE_CODE=404
+  RESPONSE_DATA=""
+  CURL_EXIT_CODE=0
+
+  ## VAULT_URL is imported from this config
+  source $ADMIRAL_ENV
+
+  touch $VAULT_API_RESPONSE_FILE
+}
+
+__vault_get() {
+  __initialize
+
+  local url="$VAULT_URL/v1/$1"
+  {
+    RESPONSE_CODE=$(curl \
+      -X GET $url \
+      -H "Content-Type: application/json" \
+      --silent --write-out "%{http_code}\n" \
+      --output $VAULT_API_RESPONSE_FILE)
+  } || {
+  CURL_EXIT_CODE=$(echo $?)
+}
+
+if [ $CURL_EXIT_CODE -gt 0 ]; then
+  # we are assuming that if curl cmd failed, vault API is unavailable
+  response="curl failed with error code $CURL_EXIT_CODE. vault might be down."
+  response_status_code=503
+else
+  response_status_code="$RESPONSE_CODE"
+  response=$(cat $VAULT_API_RESPONSE_FILE)
+fi
+
+rm -f $VAULT_API_RESPONSE_FILE
+}
+
+__vault_put() {
+  __initialize
+
+  local url="$VAULT_URL/v1/$1"
+  local update="$2"
+  {
+    RESPONSE_CODE=$(curl \
+      -X PUT $url \
+      -H "Content-Type: application/json" \
+      -d "$update" \
+      --write-out "%{http_code}\n" \
+      --silent \
+      --output $VAULT_API_RESPONSE_FILE)
+  } || {
+  CURL_EXIT_CODE=$(echo $?)
+}
+
+if [ $CURL_EXIT_CODE -gt 0 ]; then
+  # we are assuming that if curl cmd failed, vault API is unavailable
+  response="curl failed with error code $CURL_EXIT_CODE. vault might be down."
+  response_status_code=503
+else
+  response_status_code="$RESPONSE_CODE"
+  response=$(cat $VAULT_API_RESPONSE_FILE)
+fi
+
+rm -f $VAULT_API_RESPONSE_FILE
+}
+
+## Methods
+_vault_get_status() {
+  local vault_status_endpoint="sys/health"
+  __vault_get $vault_status_endpoint
+}
+
+_vault_unseal() {
+  local vault_unseal_endpoint="sys/unseal"
+  local unseal_payload="$1"
+  __vault_put $vault_unseal_endpoint "$unseal_payload"
+}
+
+##################### End Vault adapter ######################################
+
+__generate_unseal_payload() {
+  local unseal_key="$1"
+  payload='{"key": "'$unseal_key'"}'
+}
 
 __unseal_vault() {
   echo "Unsealing vault server"
   echo "Waiting $BOOT_WAIT seconds for vault server to start"
   sleep $BOOT_WAIT
 
-  source $ADMIRAL_ENV
+  _vault_get_status
+  local initialized_status=$(echo $response \
+    | jq -r '.initialized')
+  local sealed_status=$(echo $response \
+    | jq -r '.sealed')
+  echo $initialized_status
+  echo $sealed_status
 
+  if [ "$initialized_status" == "true" ]; then
+    echo "Vault has already been initialized, proceeding to unseal it"
+    if [ "$sealed_status" == "true" ]; then
+      __generate_unseal_payload "$VAULT_UNSEAL_KEY1"
+      _vault_unseal "$payload"
+      echo "Unseal response: $response"
 
-  if [ ! -z "$VAULT_UNSEAL_KEY1" ]; then
-    echo "Applying unseal key 1 to unlock vault"
-    vault unseal $VAULT_UNSEAL_KEY1
+      __generate_unseal_payload "$VAULT_UNSEAL_KEY2"
+      _vault_unseal "$payload"
+      echo "Unseal response: $response"
+
+      __generate_unseal_payload "$VAULT_UNSEAL_KEY3"
+      _vault_unseal "$payload"
+      echo "Unseal response: $response"
+
+    else
+      echo "Vault already unsealed, skipping unseal steps"
+      exit 0
+    fi
+  else
+    echo "Vault not initialized. Initialize vault before trying to unseal it"
+    exit 1
   fi
-
-  if [ ! -z "$VAULT_UNSEAL_KEY2" ]; then
-    echo "Applying unseal key 2 to unlock vault"
-    vault unseal $VAULT_UNSEAL_KEY2
-  fi
-
-  if [ ! -z "$VAULT_UNSEAL_KEY3" ]; then
-    echo "Applying unseal key 3 to unlock vault"
-    vault unseal $VAULT_UNSEAL_KEY3
-  fi
-
 }
 
 __unseal_vault
